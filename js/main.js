985f27267e2156116d36e3010ea3224753b6e16f
westerovs <westerovs@mail.ru>
2020-07-23 23:41:35 +0300

    added beautiful

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 const wrapper = document.querySelector('.wrapper');
-const empty = Array.from(document.querySelectorAll('.empty'));
 const drag = document.querySelector('.drag');
 
+let shiftX = null;
+let shiftY = null;
+
+drag.addEventListener('touchstart', touchStart);
 drag.addEventListener('touchmove', touchMove);
 drag.addEventListener('touchend', touchEnd);
+
+// ------------------------ touchStart
+function touchStart(event) {
+    event.preventDefault();
+
+    const touch = event.targetTouches[0];
+    shiftX = touch.pageX - drag.getBoundingClientRect().left;
+    shiftY = touch.pageY - drag.getBoundingClientRect().top;
+}
 
 // ------------------------ touchMove
 function touchMove(event) {
     event.preventDefault();
 
-
-    let touch = event.targetTouches[0];
-    drag.style.top = `${touch.pageY - (wrapper.offsetTop) - (drag.offsetWidth / 2)}px`;
-    drag.style.left = `${touch.pageX - (wrapper.offsetLeft) - (drag.offsetHeight / 2)}px`;
+    const touch = event.targetTouches[0];
+    drag.style.top = `${touch.pageY - (wrapper.offsetTop) - (shiftY)}px`;
+    drag.style.left = `${touch.pageX - (wrapper.offsetLeft) - (shiftX)}px`;
     drag.style.margin = '';
     drag.style.right = 'unset';
     drag.style.bottom = 'unset';
-    drag.style.backgroundImage = 'url(../img/2.jpg)';
+    drag.style.backgroundPosition = `-100px 0`;
+    drag.style.boxShadow = `5px 5px 10px gray`;
 
     if (drag.getBoundingClientRect().top <= wrapper.getBoundingClientRect().top) {
         drag.style.top = `${0}px`;
@@ -32,13 +44,9 @@ function touchMove(event) {
     if (drag.getBoundingClientRect().left <= wrapper.getBoundingClientRect().left) {
         drag.style.left = `${0}px`;
     }
-
-
 }
 
 function touchEnd(element) {
-    drag.style.backgroundImage = 'url(../img/1.jpg)';
+    drag.style.backgroundPosition = `0 0`;
+    drag.style.boxShadow = `0 0 0 black`;
 }
-
-console.log(drag.getBoundingClientRect().right);
-console.log(wrapper.getBoundingClientRect().right);
